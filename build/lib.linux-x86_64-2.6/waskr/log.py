df0824f419f9ec392dd612a2c1bc4b440ca47e61
aloneroad <aloneroad@ubuntu.ubuntu-domain>
2010-11-19 10:16:11 -0500

    update unittest

--- build/lib.linux-x86_64-2.6/waskr/log.py
+++
@@ -1,9 +0,0 @@
-from logging import getLogger
-
-## Set logger objects for all modules
-middleware  = getLogger('waskr.middleware')
-util        = getLogger('waskr.util')
-database    = getLogger('waskr.database')
-server      = getLogger('waskr.web.server')
-model       = getLogger('waskr.web.model')
-
--- waskr/tests/test_request_stats.py
+++ waskr/tests/test_request_stats.py
@@ -40,22 +40,21 @@ class TestRequestStatsMiddleware(unittest.TestCase):
         application = RequestStatsMiddleware(app=None)
         actual = application.config 
         expected = {
-                'db_engine': 'sqlite',
-                'db_location': '/tmp',
-                'server_id': '1', 
-                'web_port': '8080', 
-                'db_port': 27017, 
-                'application': 'main', 
-                'db_host': 'localhost', 
-                'web_host': 'localhost',
-                'web_user':False,
-                'cache': '10',
-                'plugin_path': False,
-                'plugins':None,
-                'log_level': 'DEBUG',
-                'log_format': '%(asctime)s %(levelname)s %(name)s  %(message)s',
-                'log_datefmt' : '%H:%M:%S'
-                }
+            'server_id': '1',
+            'db_engine': 'mongodb',
+            'db_host': 'localhost',
+            'db_port': 27017,
+            'application': 'main',
+            'web_host': 'localhost',
+            'web_port': '8081',
+            'web_user': 'foo@bar.com',
+            'cache': '10',
+            'plugins': None,
+            'plugin_path': False,
+            'log_level': 'DEBUG',
+            'log_format': '%(asctime)s %(levelname)s %(name)s  %(message)s',
+            'log_datefmt' : '%H:%M:%S'
+            }
         self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
